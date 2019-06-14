18306372f1b58216315ea812b1c152c766aadc87
likey0u96 <kyouj96@gmail.com>
2019-06-14 18:42:41 +0900

    이진트리 과제 문제 수정
    이진트리에서 높이를 경로에 포함된 에지의 갯수였는데,
    노드의 갯수로 변경됨

--- data_structure_class/binary_tree_height.cpp
+++ data_structure_class/binary_tree_height.cpp
@@ -1,21 +1,6 @@
 /*
 2016125086 김유진
 UTF-8      C++     MAC
-
-과제에서 주어진 예제(example)는 
-input : 6
-      3 1 2 4 5 6
-output : 4      이다.
-
-이는 트리(tree)로 만들면 
-      3
-    1    4
-      2    5
-              6   이므로 과제처럼 높이(height)를 가장 긴 경로를 잇는 에지(edge)의 갯수라고 정의한다면
-                              높이(height)는 3이다.
-    
-    검색결과 트리의 높이는 루트로부터 가장 멀리 떨어진 노드와의 거리로 정의된다.
-    이를 기준으로 작성하였다.
  */
 
 #include <iostream>
@@ -41,7 +26,7 @@ void insert_node( NODE **t, int key)
     NODE *n     = (NODE *)malloc(sizeof(NODE));
     NODE *temp  = *t; // temporary node.
 
-    h = 0;   //init height
+    h = 1;   //init height
     n->key  = key;
     n->p    = NULL;
     n->left = NULL;
